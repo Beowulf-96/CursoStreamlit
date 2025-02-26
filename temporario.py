750624f1251f436492f47e2aa67f70a81d1d633b
Beowulf-96 <isacraphael4@gmail.com>
2025-02-26 20:01:14 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -4,6 +4,6 @@ st.toggle("abc")
 st.text_area("Enter text")
 st.text_input("abc")
 st.selectbox(
-  'Qual sua cor favorita',
+  'Qual sua cor favorita?',
   ('Azul', 'Vermelho', 'Verde'))
 st.button("Botão Salvar")
