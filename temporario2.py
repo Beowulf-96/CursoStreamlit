a470b74fbf064d502f31c2e29a0f50a0d34c52e1
Beowulf-96 <isacraphael4@gmail.com>
2025-02-28 20:19:17 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -6,10 +6,13 @@ st.write("Preencha o formulário abaixo para cadastrar um aluno:")
 
 with st.form(key='form_cadastro'):
     nome = st.text_input("Nome do Aluno")
-    idade = st.number_input("Idade", min_value=1, max_value=100, step=1)
+    CPF = st.number_input("Idade", min_value=1, max_value=100, step=1)
     curso = st.text_input("Curso")
     email = st.text_input("Email")
     matricula = st.text_input("Número de Matrícula")
+    Pai = st.text_input("Pai")
+    Mãe = st.text_input("Mãe")
+    Nascimento = st.number_input("Idade", min_value=1, max_value=100, step=1)
     
    
     semestre = st.selectbox("Semestre", ["1º Semestre", "2º Semestre", "3º Semestre", "4º Semestre", "5º Semestre"])
