e4f7a6bfa8152b6737768f2419d40ef22bbd799b
Beowulf-96 <isacraphael4@gmail.com>
2025-02-28 20:23:16 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -6,13 +6,24 @@ st.write("Preencha o formulário abaixo para cadastrar um aluno:")
 
 with st.form(key='form_cadastro'):
     nome = st.text_input("Nome do Aluno")
-    CPF = st.number_input("Idade", min_value=1, max_value=100, step=1)
     curso = st.text_input("Curso")
     email = st.text_input("Email")
     matricula = st.text_input("Número de Matrícula")
     Pai = st.text_input("Pai")
     Mãe = st.text_input("Mãe")
-    Nascimento = st.number_input("Nascimento", min_value=1, max_value=100, step=1)
+    cpf = st.text_input('Digite seu CPF:', '')
+
+if cpf and len(cpf) != 11:
+    st.error('CPF deve conter 11 dígitos.')
+
+data_nascimento = st.date_input('Escolha sua data de nascimento:')
+
+if st.button('Enviar'):
+    if cpf and len(cpf) == 11:
+        st.write(f'CPF: {cpf}')
+        st.write(f'Data de Nascimento: {data_nascimento}')
+    else:
+        st.error('Preencha corretamente o CPF.')
     
    
     semestre = st.selectbox("Semestre", ["1º Semestre", "2º Semestre", "3º Semestre", "4º Semestre", "5º Semestre"])
