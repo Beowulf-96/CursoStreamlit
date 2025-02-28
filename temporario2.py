d85e54cdc531a7a73c5ec4cba9987d2f173bd190
Beowulf-96 <isacraphael4@gmail.com>
2025-02-28 20:05:37 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -3,3 +3,9 @@ import streamlit as st
 import streamlit as st
 
 st.text("This is text\n[and more text](that's not a Markdown link).")
+
+st.text_area("Enter text")
+
+st.text_area("Enter text")
+
+st.text_area("Enter text")
