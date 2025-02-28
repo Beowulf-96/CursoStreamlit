a9f2d0c6c8ca7196c056f003bc5fa90198fa38d3
Beowulf-96 <isacraphael4@gmail.com>
2025-02-28 20:28:07 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -24,6 +24,5 @@ with st.form(key='form_cadastro'):
         st.write(f"Curso: {curso}")
         st.write(f"Email: {email}")
         st.write(f"Matrícula: {matricula}")
-        st.write(f"Semestre: {semestre}")
         st.write(f"Semestre: {cpf}")
 
