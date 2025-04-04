887510f3f58b21e53d4a87b6da391b7ce4eba1f3
Beowulf-96 <isacraphael4@gmail.com>
2025-04-04 19:43:12 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -3,11 +3,11 @@ st.title('Cadastro de Funcionário')
 nome = st.text_input("Digite o nome:")
 sobrenome = st.text_input("Digite o sobrenome:")
 dia = st.number_input("Digite o dia do nascimento:")
-mes = st.number_input("Digite o mês do nascimento:", min_value=1, max_value=12)
-ano = st.number_input("Digite o ano do nascimento:", min_value=1900, max_value=2025)
+mes = st.number_input("Digite o mês do nascimento:")
+ano = st.number_input("Digite o ano do nascimento:")
 st_civil = st.selectbox("Digite o estado civil:", ['Solteiro(a)', 'Casado(a)', 'Divorciado(a)', 'Viúvo(a)'])
 sexo = st.selectbox("Digite o sexo:", ['Masculino', 'Feminino', 'Outro'])
-salario = st.number_input("Digite o salário:", min_value=0)
+salario = st.number_input("Digite o salário:")
 aumento = 0
 salario_final = 0
 if salario <= 2500:
